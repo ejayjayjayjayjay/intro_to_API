541d03bdd0a404e79e37b4b096f188cbd8ee790e
Josell Vibar <vibarjosell@gmail.com>
2022-10-21 19:23:08 +0800

    ..

--- weatherApp/index.js
+++ weatherApp/index.js
@@ -9,13 +9,22 @@
 
 
 const express = require('express')
+const https = require('node:https');
+
 const app = express()
 const port = 3000
 
+
+
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  const url = "https://api.openweathermap.org/data/2.5/weather?q=manila&units=metric&appid=9f7e9e0b314b24c7c6eaf82ebc88862a";
+  https.get(url, function(response){
+    console.log(response);
+  });
+  res.send('Server is running') 
 })
 
 app.listen(port, () => {
-  console.log(`Server is running on port ${port}`)
+  console.log(`Server is running on ${port}`)
 })
+
